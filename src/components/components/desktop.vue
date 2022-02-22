e38489fbcff0728e64d2d0183b3b45c28ba75764
lewism <lewis@sendy.co.ke>
2022-02-22 15:58:34 +0300

    Add dummy UI

--- src/components/components/desktop.vue
+++ src/components/components/desktop.vue
@@ -1,10 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>Desktop</div>
+  <div>
+    <div class="side-panel-left">
+      <div>Dashboard</div>
+      <div>Inventory</div>
+      <div>Delivery requests</div>
+      <div>Settings</div>
+    </div>
+    <div>
+      <div>Header</div>
+      <div>
+        <div>Timeline</div>
+        <div>Info Bar</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.side-panel-left {
+  height: 100%;
+}
+</style>

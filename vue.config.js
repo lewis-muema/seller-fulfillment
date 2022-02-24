9cbb9da6c04da1144b518b60caf42af42e59fef9
lewism <lewis@sendy.co.ke>
2022-02-24 16:39:24 +0300

    Re-add environment configs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+const variables = require(`./config/${process.env.DOCKER_ENV}`);
+
 module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new webpack.EnvironmentPlugin(variables)],
+  },
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader

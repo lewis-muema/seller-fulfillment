78eeb45d92d847d3417e535ce8ab55e05a45d25d
lewism <lewis@sendy.co.ke>
2022-05-06 14:59:33 +0300

    Add payments flow

--- locales/locales/fr.js
+++ locales/locales/fr.js
@@ -1,9 +1,11 @@
 import * as common from "../common/fr.json";
 import * as deliveries from "../deliveries/fr.json";
+import * as payments from "../payments/fr.json";
 
 const translations = {
   common: common.default,
   deliveries: deliveries.default,
+  payments: payments.default,
 };
 
 export default translations;
---
+++ src/modules/payments/components/inVoiceDetails.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="invoices-details-container">
+    <div>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.billedFrom") }}
+        </span>
+      </p>
+      <p v-for="(item, i) in billedFrom" :key="i">
+        <span :class="$store.getters.getLoader">
+          {{ item }}
+        </span>
+      </p>
+    </div>
+    <div>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.billedTo") }}
+        </span>
+      </p>
+      <p v-for="(item, i) in billedTo" :key="i">
+        <span :class="$store.getters.getLoader">
+          {{ item }}
+        </span>
+      </p>
+    </div>
+    <div>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.invoiceDetails") }}
+        </span>
+      </p>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.invoiceNumber") }}: {{ invoiceDetails.invoiceNumber }}
+        </span>
+      </p>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.dateSent") }}: {{ invoiceDetails.dateSent }}
+        </span>
+      </p>
+      <p>
+        <span :class="$store.getters.getLoader">
+          {{ $t("payments.dueDate") }}: {{ invoiceDetails.dueDate }}
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      billedFrom: [
+        "Sendy Kenya Marketplace limited",
+        "Marsabit Plaza",
+        "P.O. BOX 10441",
+        "00100 Nairobi",
+      ],
+      billedTo: ["Smart Collections", "Kikuyu Road"],
+      invoiceDetails: {
+        invoiceNumber: 9459494,
+        dateSent: "3/1/2022",
+        dueDate: "6/1/2022",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.invoices-details-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  margin: 30px;
+  padding: 35px;
+  font-size: 14px;
+  line-height: 25px;
+  border: 1px solid #e2e7ed;
+  border-radius: 10px;
+}
+</style>

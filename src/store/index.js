518f821e4f793a79b5052a47410510be450561c4
lewis-muema <lewis@sendy.co.ke>
2023-02-28 12:07:07 +0300

    Add flag to hide LPO upload feature and add error handling

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,7 @@ export default createStore({
       ],
       autofillDetails: {},
       autoFillVariants: [],
+      LPOUploadError: "",
       autofillReviewStatus: false,
       deliverySpeed: [
         {

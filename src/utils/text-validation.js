a9b9410ebc6746d0ad73a47afb454ae72a3a7616
enkuria <eugene@sendyit.com>
2023-03-03 12:14:20 +0300

    refactor: require https or http protocol for url validation

--- src/utils/text-validation.js
+++ src/utils/text-validation.js
@@ -1,6 +1,6 @@
 //Checks if a string is a url value
 export function isValidUrl(text) {
   var pattern =
-    /^(?:https?:\/\/)?(?:[\w_-]+(?:\.[\w_-]+)\.){1,}(?:[a-zA-Z]{2,63})(?:\/[\w@?^=%&/~+#-]*)?$/i;
+    /^(?:(https|http):\/\/)(?:[\w_-]+(?:\.[\w_-]+)\.){1,}(?:[a-zA-Z]{2,63})(?:\/[\w@?^=%&/~+#-]*)?$/i;
   return pattern.test(text);
 }

3e5ac789d3d9784017cd162bdab244a291553d54
enkuria <eugene@sendyit.com>
2023-03-07 14:58:28 +0300

    refactor: change regex to accept urls without a sub-domain

--- src/utils/text-validation.js
+++ src/utils/text-validation.js
@@ -1,6 +1,5 @@
 //Checks if a string is a url value
 export function isValidUrl(text) {
-  var pattern =
-    /^(?:(https|http):\/\/)(?:[\w_-]+(?:\.[\w_-]+)\.){1,}(?:[a-zA-Z]{2,63})(?:\/[\w@?^=%&/~+#-]*)?$/i;
+  var pattern = /^(?:(https|http):\/\/)(?:[\w_-]+(?:\.[\w_-]+)){1,}/i;
   return pattern.test(text);
 }

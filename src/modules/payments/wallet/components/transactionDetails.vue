1677dd7ec04336501dd71cfb9be1bc598a553ac3
dorcasChero <dorcas@sendyit.com>
2023-04-24 11:16:17 +0300

    SFT-496 resolve merge conflicts

--- src/modules/payments/wallet/components/transactionDetails.vue
+++ src/modules/payments/wallet/components/transactionDetails.vue
@@ -197,37 +197,6 @@
             >
           </div>
         </div>
-        <!-- <div class="transaction-amount" v-else>
-          <span>
-            {{ $t("payments.amount") }}
-            <i
-              v-if="
-                [
-                  'UPCOMING_EARNING_FROM_SALE_OF_GOOD',
-                  'EARNING_FROM_SALE_OF_GOOD',
-                ].includes(getActiveTransaction.transaction_type)
-              "
-              class="mdi mdi-alert-circle-outline"
-              @click="
-                setOverlayStatus({
-                  overlay: true,
-                  popup: 'paymentBreakdown',
-                })
-              "
-            ></i>
-          </span>
-          <span
-            :class="
-              getSignMapping[getActiveTransaction.transaction_type] === '+'
-                ? 'transaction-amount-right'
-                : 'transaction-amount-right-negative'
-            "
-          >
-            {{ getSignMapping[getActiveTransaction.transaction_type] }}
-            {{ getActiveTransaction.transaction_currency }}
-            {{ getActiveTransaction.transaction_amount }}
-          </span>
-        </div > -->
         <div
           v-if="
             [

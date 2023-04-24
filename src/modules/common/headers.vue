f0cefd71f5c4c83e82fe96e59401d51735c38c29
lewis-muema <lewis@sendy.co.ke>
2023-04-24 13:16:07 +0300

    Change on demand to hire vehicle

--- src/modules/common/headers.vue
+++ src/modules/common/headers.vue
@@ -532,6 +532,7 @@ export default {
     0px 0px 10px 0px rgb(0 0 0 / 14%), 0px 0px 10px 0px rgb(0 0 0 / 12%) !important;
   padding: 5px 10px;
   margin-top: 5px;
+  max-height: 90vh;
 }
 .header-profile-popup {
   box-shadow: 0px 0px 2px 0px rgb(0 0 0 / 20%),

dd195b9550ed1dea6c0a2f402f249d9b396939b1
dorcasChero <dorcas@sendyit.com>
2023-05-11 13:58:08 +0300

    remove console log

--- src/mixins/algolia_search.js
+++ src/mixins/algolia_search.js
@@ -29,7 +29,7 @@ const algoliaInit = {
           this.algoliaResults(objects);
         })
         .catch((error) => {
-          console.log("error", error);
+          console.log(error);
         });
     },
   },
